6922e7c95d486e7a5fba83b182a7e43238f26923
RIVIERE <poupine.974@gmail.com>
2012-01-07 17:54:25 +0100

    plus de fichier java générés par le compilo

--- MicroJava1112/src/mjc/egg/IMJAVAMessages.java
+++
@@ -1,8 +0,0 @@
-package mjc.egg;
-public interface IMJAVAMessages {
-
-  public static  int id_MJAVA_expected_token = 5505024;
-  public static  int id_MJAVA_unexpected_token = 5505025;
-  public static  int id_MJAVA_expected_eof = 5505026;
-  public static  int id_err_ident_already_known = 5505027;
-  }

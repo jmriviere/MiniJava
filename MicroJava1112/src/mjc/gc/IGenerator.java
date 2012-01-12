ddfc37555439d5dff8de7fde70c0bafeb3259ecd
Emmanuel Bigeon <emmanuel.bigeon@laposte.net>
2012-01-12 19:25:44 +0100

    Q devrait marcher... il a ete entierement refait...

---
+++ MicroJava1112/src/mjc/gc/IGenerator.java
@@ -0,0 +1,71 @@
+package mjc.gc;
+
+public interface IGenerator {
+
+	String concatenateArgument(String att_code, String att_code2);
+
+	String generateBloc(String att_code);
+
+	Object generateConstructor(String att_code);
+
+	String generateClazz(Clazz glob_4_clazz);
+
+	String generateAffectation(String att_code, String att_code2);
+
+	String concatenateEntities(String att_code, String att_code2);
+
+	String generateRelativeOperation(String att_lcode, String att_code,
+			String att_code2);
+
+	String generateAdditionOperation(String att_lcode, String att_code,
+			String att_code2);
+
+	String generateUnaryOperation(String att_code, String att_code2);
+
+	String generateBoolean(boolean b);
+
+	String generateInteger(String att_txt);
+
+	String generateNull();
+
+	String generateInstance(Clazz glob_72_clazz, Signature att_signature);
+
+	String generateReturn(String att_code);
+
+	String generateIfThenElse(String att_code, String att_code2,
+			String att_code3);
+
+	String generateSequence(String att_code, String att_code2);
+
+	String generateMinus();
+
+	String generateOr();
+
+	String generatePlus();
+
+	String generateAnd();
+
+	String generateMult();
+
+	String generateMod();
+
+	String generateDiv();
+
+	String generateNEq();
+
+	String generateEq();
+
+	String generateInfEq();
+
+	String generateSupEq();
+
+	String generateInf();
+
+	String generateSup();
+	
+	String generateNot();
+
+	String generateMultiplicationOperation(String att_lcode, String att_code,
+			String att_code2);
+
+}
---
+++ MicroJava1112/src/mjc/gc/MockGenerator.java
@@ -0,0 +1,195 @@
+package mjc.gc;
+
+public class MockGenerator implements IGenerator {
+
+	@Override
+	public String concatenateArgument(String att_code, String att_code2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateBloc(String att_code) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object generateConstructor(String att_code) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateClazz(Clazz glob_4_clazz) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateAffectation(String att_code, String att_code2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String concatenateEntities(String att_code, String att_code2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateRelativeOperation(String att_lcode, String att_code,
+			String att_code2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateAdditionOperation(String att_lcode, String att_code,
+			String att_code2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateUnaryOperation(String att_code, String att_code2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateBoolean(boolean b) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateInteger(String att_txt) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateNull() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateInstance(Clazz glob_72_clazz, Signature att_signature) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateReturn(String att_code) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateIfThenElse(String att_code, String att_code2,
+			String att_code3) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateSequence(String att_code, String att_code2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateMinus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateOr() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generatePlus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateAnd() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateMult() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateMod() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateDiv() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateNEq() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateEq() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateInfEq() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateSupEq() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateInf() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateSup() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateNot() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateMultiplicationOperation(String att_lcode,
+			String att_code, String att_code2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
